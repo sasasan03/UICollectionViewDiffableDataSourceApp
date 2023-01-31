ff8c992d911aee87074e8e0512123cf42163c13f
0102johnny <rt2824.recruit.1022@gmail.com>
2023-01-31 18:00:49 +0900

    パースしたデータを格納するModelの値として新たにポケモンの説明文を追加

--- SampleUICollectionViewDiffableDataSource/Pokemon.swift
+++ SampleUICollectionViewDiffableDataSource/Pokemon.swift
@@ -15,25 +15,48 @@ struct Pokemon: Decodable {
     let id: Int
     // ポケモンの画像
     let sprites: Image
-    // 画像のデータ構造
-    struct Image: Decodable {
-        // ポケモンが正面向きの画像
-        let frontImage: String
-
-        // デコードの際の代替キーをfrontImageプロパティにセット
-        enum CodingKeys: String, CodingKey {
-            case frontImage = "front_default"
-        }
-    }
     // ポケモンのタイプ
     let types: [TypeEntry]
+    // ポケモンの説明文
+    let species: [SpeciesReference]
+}
 
-    struct TypeEntry: Decodable {
-      let type: Mode
+// 画像のデータ構造
+struct Image: Decodable {
+    // ポケモンが正面向きの画像
+    let frontImage: String
 
-        // Typeが命名で利用できず、他に適切な表現が思い浮かばなかった。
-      struct Mode: Decodable {
-        let name: String
-      }
+    // デコードの際の代替キーをfrontImageプロパティにセット
+    enum CodingKeys: String, CodingKey {
+        case frontImage = "front_default"
     }
+}
+
+// ポケモンのタイプ
+struct TypeEntry: Decodable {
+  let type: Mode
+}
+
+// ポケモンの説明文のリンク
+struct SpeciesReference: Decodable {
+    let url: String
+  }
+
+// "Type"が命名で利用できず、他に適切な表現が思い浮かばなかった。
+struct Mode: Decodable {
+let name: String
+}
+
+//　ポケモンの説明文のリンク先で取得した値を格納するためのModel
+struct Species: Decodable {
+  let flavorTextEntries: [FlavorText]
+
+  struct FlavorText: Decodable {
+    let flavorText: String
+    let language: Language
+
+    struct Language: Decodable {
+      let name: String
+    }
+  }
 }
