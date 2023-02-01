09daf8971b7c65bca256046e52595af97e705b68
0102johnny <rt2824.recruit.1022@gmail.com>
2023-02-01 09:24:18 +0900

    ポケモン一覧に表示するCellのViewを作成

---
+++ SampleUICollectionViewDiffableDataSource/PokemonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonCell.swift
+//  SampleUICollectionViewDiffableDataSource
+//
+//  Created by Johnny Toda on 2023/02/01.
+//
+
+import UIKit
+
+final class PokemonCell: UICollectionViewCell {
+    @IBOutlet private weak var iconView: UIImageView!
+    @IBOutlet private weak var nameLabel: UILabel!
+}
