b53208d5d608fc365fd78103a4430cecef6a5793
0102johnny <rt2824.recruit.1022@gmail.com>
2023-02-01 11:25:24 +0900

    ポケモン一覧画面のViewを作成

---
+++ SampleUICollectionViewDiffableDataSource/PokemonListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PokemonListViewController.swift
+//  SampleUICollectionViewDiffableDataSource
+//
+//  Created by Johnny Toda on 2023/02/01.
+//
+
+import UIKit
+
+final class PokemonListViewController: UIViewController {
+    @IBOutlet private weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
