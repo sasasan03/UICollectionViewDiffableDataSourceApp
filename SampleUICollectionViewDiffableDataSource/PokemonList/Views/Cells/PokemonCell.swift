86b2b4e2f648b58cc16646c7cb67ccb3c0da67c3
0102johnny <rt2824.recruit.1022@gmail.com>
2023-02-09 23:07:42 +0900

    CollectionViewのレイアウトを構築する処理を実装

--- SampleUICollectionViewDiffableDataSource/PokemonList/Views/Cells/PokemonCell.swift
+++ SampleUICollectionViewDiffableDataSource/PokemonList/Views/Cells/PokemonCell.swift
@@ -6,8 +6,17 @@
 //
 
 import UIKit
+import Kingfisher
 
 final class PokemonCell: UICollectionViewCell {
     @IBOutlet private weak var iconView: UIImageView!
     @IBOutlet private weak var nameLabel: UILabel!
+
+    static let nib = UINib(nibName: String(describing: PokemonCell.self), bundle: nil)
+    static let identifier = String(describing: PokemonCell.self)
+
+    func configure(imageURL: String, name: String) {
+        iconView.kf.setImage(with: URL(string: imageURL))
+        nameLabel.text = name
+    }
 }
--- SampleUICollectionViewDiffableDataSource/PokemonList/Views/PokemonListViewController.swift
+++ SampleUICollectionViewDiffableDataSource/PokemonList/Views/PokemonListViewController.swift
@@ -9,8 +9,6 @@ import UIKit
 
 final class PokemonListViewController: UIViewController {
     @IBOutlet private weak var collectionView: UICollectionView!
-
-    static let storyboardName = "PokemonList"
     
     // PresenterはSceneDelegateにて初期化
     var presenter: PokemonListPresenterInput!
@@ -20,19 +18,76 @@ final class PokemonListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpCollectionView()
+        presenter.viewDidLoad(collectionView: collectionView)
+    }
+
+    // Cellのレイアウトを構築
+    private func setUpCollectionView() {
+        configureHierarchy()
     }
 }
 
-extension PokemonListViewController: PokemonListPresenterOutput {
-    func startIndicator() {
-        <#code#>
-    }
 
-    func updateView() {
-        <#code#>
+extension PokemonListViewController {
+    private func configureHierarchy() {
+        // CollectionViewのLayoutを実装
+        collectionView.collectionViewLayout = createLayout()
+        // XIBファイルCellをCollectionViewに登録
+        collectionView.register(PokemonCell.nib, forCellWithReuseIdentifier: PokemonCell.identifier)
+        collectionView.register(PokemonTypeCell.nib, forCellWithReuseIdentifier: PokemonTypeCell.identifier)
     }
+}
 
-    func showAlertMessage(errorMessage: String) {
-        <#code#>
+// CollectionViewのLayoutを定義
+extension PokemonListViewController {
+    private func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int,
+            layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+
+            guard let sectionKind = Section(rawValue: sectionIndex) else { return nil }
+            // Sectionごとの列数を代入
+            let columns = sectionKind.columnCount
+
+            // Itemのサイズを設定
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                 heightDimension: .fractionalHeight(1.0))
+
+            // Itemを生成
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            // Itemの上下左右間隔を指定
+            item.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
+            // 列が1だった場合、CollectionViewの幅の20％の数値を返し、それ以外はCollectionViewの高さ40%の値を返す
+            let groupHeight = columns == 1 ? NSCollectionLayoutDimension.fractionalWidth(0.2) : NSCollectionLayoutDimension.fractionalHeight(0.4)
+            // 列が1だった場合、CollectionViewの幅の20％の数値を返し、それ以外はCollectionViewの幅の値を返す
+            let groupWidth = columns == 1 ? NSCollectionLayoutDimension.fractionalWidth(0.2) : NSCollectionLayoutDimension.fractionalWidth(1.0)
+            // Groupのサイズを設定
+            let groupSize = NSCollectionLayoutSize(widthDimension: groupWidth,
+                                                   heightDimension: groupHeight)
+            // Groupを生成
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: columns)
+
+            // Sectionを生成
+            let section = NSCollectionLayoutSection(group: group)
+
+            // Sectionの上下左右間隔を指定
+            section.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
+            // Sectionごとにスクロール方向を設定
+            section.orthogonalScrollingBehavior = sectionKind.orthgonalScrollingBehavior
+            return section
+        }
+        return layout
+    }
+}
+
+extension Section {
+    // SectionごとのScroll方向を返す ※これは描画処理な気もするので
+    var orthgonalScrollingBehavior: UICollectionLayoutSectionOrthogonalScrollingBehavior {
+        switch self {
+        case .typeOfPokemonList:
+            return .continuous
+        case .pokemonList:
+            return .none
+        }
     }
 }
