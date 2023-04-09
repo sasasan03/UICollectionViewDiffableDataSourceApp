146839584bf3e28ef18fbf9e16ba318e8e516e48
0102johnny <rt2824.recruit.1022@gmail.com>
2023-04-09 23:34:42 +0900

    PokemonListのPokemonCellをタップして遷移する詳細画面のViewをStoryboardで作成

---
+++ SampleUICollectionViewDiffableDataSource/PokemonListDetails/PokemonListDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PokemonListDetailsViewController.swift
+//  SampleUICollectionViewDiffableDataSource
+//
+//  Created by Johnny Toda on 2023/04/09.
+//
+
+import UIKit
+
+final class PokemonListDetailsViewController: UIViewController {
+    @IBOutlet private weak var iconView: UIImageView!
+    @IBOutlet private weak var nameLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
