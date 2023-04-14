f8d75bdf0e0058d6402ae20fb7a8efe5dd3e3353
0102johnny <rt2824.recruit.1022@gmail.com>
2023-04-14 18:15:46 +0900

    Controllerにpresenterに依頼する処理を追加

--- SampleUICollectionViewDiffableDataSource/PokemonDetails/PokemonDetailsViewController.swift
+++ SampleUICollectionViewDiffableDataSource/PokemonDetails/PokemonDetailsViewController.swift
@@ -6,13 +6,36 @@
 //
 
 import UIKit
+import Kingfisher
 
 final class PokemonDetailsViewController: UIViewController {
     @IBOutlet private weak var iconView: UIImageView!
     @IBOutlet private weak var nameLabel: UILabel!
 
+    // PresenterはSceneDelegateにて初期化
+    var presenter: PokemonDetailsPresenterInput!
+    func inject(presenter: PokemonDetailsPresenterInput) {
+        self.presenter = presenter
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // presenterにviewDidLoadを定義してここで呼びだす？
+        // presenterに処理を依頼
+        presenter.viewDidLoad()
+    }
+
+    // 遷移元に戻る際、インスタンスがメモリから解放されているかをチェック
+    deinit {
+        print(String(describing: PokemonDetailsViewController.self) + " is deinitialized.")
+    }
+}
+
+extension PokemonDetailsViewController: PokemonDetailsPresenterOutput {
+    // presenterから描画指示を受けて処理を実行
+    func configure(pokemon: Pokemon?) {
+        // 🍎ここに関しては強制終了させずとも、エラー時の代替データを表示させる処理をthrowする方向で良いかも？
+        guard let pokemon = pokemon else { fatalError("unexpected error") }
+        iconView.kf.setImage(with: URL(string: pokemon.sprites.frontImage))
+        nameLabel.text = pokemon.name
     }
 }
