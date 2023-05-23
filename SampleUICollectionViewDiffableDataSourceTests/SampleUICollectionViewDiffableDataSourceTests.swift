3d55e0b925dabe4c2560f32f7bedf1b7243929b5
0102johnny <rt2824.recruit.1022@gmail.com>
2023-05-23 12:00:53 +0900

    UnitTestの内容を改善

--- SampleUICollectionViewDiffableDataSourceTests/SampleUICollectionViewDiffableDataSourceTests.swift
+++ SampleUICollectionViewDiffableDataSourceTests/SampleUICollectionViewDiffableDataSourceTests.swift
@@ -12,8 +12,16 @@ final class SampleUICollectionViewDiffableDataSourceTests: XCTestCase {
     // データ取得をテスト
     func testFetchPokemonsData() async {
         let api = API()
+        // 非同期処理の待機と完了を表現する為のインスタンス
+        let exp = XCTestExpectation()
+
         let expectedPokemonsCount = 151
         let pokemons = await api.asyncFetchPokemonData()
         XCTAssertEqual(pokemons.count, expectedPokemonsCount)
+        // 非同期処理が完了時に実行される
+        exp.fulfill()
+
+        // expのfullfill()が実行されない場合、テストが失敗となる？
+        await fulfillment(of: [exp])
     }
 }
