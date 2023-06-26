67026dc23fa0fbe173d2f2b0fb6a1aa9e355ac0b
0102johnny <rt2824.recruit.1022@gmail.com>
2023-06-27 07:03:34 +0900

    [refactor] HTTPErrorを管理するeunmの作成に伴い、MockAPIのurlErrorをHTTPErrorに差し替え

--- SampleUICollectionViewDiffableDataSource/Dummy/MockAPI.swift
+++ SampleUICollectionViewDiffableDataSource/Dummy/MockAPI.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct MockAPI: APIInput {
-    var urlError: URLError?
+    var httpError: HTTPError?
     var apiError: APIError?
 
     // ダミーデータを定義
@@ -19,7 +19,7 @@ struct MockAPI: APIInput {
 //    let pikachu = sampleData.pikachu
 
     func decodePokemonData(completion: @escaping (Result<[Pokemon], Error>) -> Void) {
-        if let urlError { completion(.failure(urlError)) }
+        if let httpError { completion(.failure(httpError)) }
         if let apiError { completion(.failure(apiError)) }
         // ここに渡すデータが固定ではなく、変動して欲しい
         completion(.success(mockPokemonData ?? []))
