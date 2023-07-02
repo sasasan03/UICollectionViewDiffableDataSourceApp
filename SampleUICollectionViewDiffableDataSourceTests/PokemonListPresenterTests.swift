94f69b72677a71bb9869c911588dd99f6cb71679
0102johnny <rt2824.recruit.1022@gmail.com>
2023-07-02 23:48:20 +0900

    [refactor] プロジェクトのリファクタリングに伴い、PokemonListPresenterのテストコードを編集

--- SampleUICollectionViewDiffableDataSourceTests/PokemonListPresenterTests.swift
+++ SampleUICollectionViewDiffableDataSourceTests/PokemonListPresenterTests.swift
@@ -16,44 +16,20 @@ final class PokemonListPresenterTests: XCTestCase {
         let mockPokemonListView = MockPokemonListView()
         let presenter = PokemonListPresenter(view: mockPokemonListView, model: mockAPI)
 
-        // 仮想通信を実行？
-        //presenter.model.decodePokemonData(completion: { [weak self] result in
-        //    switch result {
-        //    case .success(let pokemonsData):
-        //        // 順次要素を追加
-        //        pokemonsData.forEach {
-        //            self?.pokemons.append(Item(pokemon: $0))
-        //        }
-        //
-        //        // ポケモン図鑑No.の昇順になるよう並び替え
-        //        self?.pokemons.sort {
-        //            guard let pokedexNumber = $0.pokemon else { fatalError("unexpectedError") }
-        //            guard let anotherPokedexNumber = $1.pokemon else { fatalError("unexpectedError") }
-        //            return pokedexNumber.id < anotherPokedexNumber.id
-        //        }
-        //
-        //        // Setは要素を一意にする為、一度追加されたタイプを自動で省いてくれる。(例: フシギダネが呼ばれると草タイプと毒タイプを取得するので次のフシギソウのタイプは追加されない。
-        //        // 結果としてタイプリストの重複を避けることができる
-        //        self?.pokemons.forEach {
-        //            $0.pokemon?.types.forEach { self?.pokemonTypes.insert($0.type.name) }
-        //        }
-        //        DispatchQueue.main.async {
-        //            self?.applyInitialSnapshots()
-        //            self?.view.updateView()
-        //        }
-        //    case .failure(let error as URLError):
-        //        DispatchQueue.main.async {
-        //            self?.view.showAlertMessage(errorMessage: error.message)
-        //        }
-        //    case .failure:
-        //        fatalError("unexpected Errorr")
-        //    }
-        //})
+        // 仮想通信を実行
+        presenter.viewDidLoad()
     }
 
+    
     func testEmptyPokemonsList() {
         // デフォルト引数でnilが入り、二項演算子でPokemonデータがnilである場合は空の配列を返す
+        // 1体のポケモンデータが含まれたデータ配列を代入
         let mockAPI = MockAPI()
+        let mockPokemonListView = MockPokemonListView()
+        let presenter = PokemonListPresenter(view: mockPokemonListView, model: mockAPI)
+
+        // 仮想通信を実行
+        presenter.viewDidLoad()
     }
 
     // 5体のポケモンデータが取得できているかテスト
