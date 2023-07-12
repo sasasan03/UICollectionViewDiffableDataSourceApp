c2434c356ba745b6428c161ad84d5e2a5ce866bc
0102johnny <rt2824.recruit.1022@gmail.com>
2023-07-12 11:22:44 +0900

    [refactor] 通信処理と非同期処理をfetchPokemonsメソッドとして定義し、viewDidLoadメソッドとdidTapRestartURLSessionButton改めdidTapRetryButtonで実行する仕様にhennkou

--- SampleUICollectionViewDiffableDataSource/PokemonList/Presenter/PokemonListPresenter.swift
+++ SampleUICollectionViewDiffableDataSource/PokemonList/Presenter/PokemonListPresenter.swift
@@ -66,8 +66,7 @@ final class PokemonListPresenter: PokemonListPresenterInput {
         print(String(describing: PokemonListPresenter.self) + " " + "is deinitialized.")
     }
 
-    // アプリ起動時にviewから通知
-    func viewDidLoad() {
+    private func fetchPokemons() {
         view.startIndicator()
         model.decodePokemonData(completion: { [weak self] result in
             switch result {
@@ -108,47 +107,14 @@ final class PokemonListPresenter: PokemonListPresenterInput {
         })
     }
 
+    // アプリ起動時にviewから通知
+    func viewDidLoad() {
+        fetchPokemons()
+    }
+
     /// 再度通信処理を実行
     func didTapRestartURLSessionButton() {
-        view.startIndicator()
-        model.decodePokemonData(completion: { [weak self] result in
-            switch result {
-            case .success(let pokemonsData):
-                DispatchQueue.main.async {
-                    // 順次要素を追加
-                    pokemonsData.forEach {
-                        self?.pokemons.append(ListItem.pokemon($0))
-                    }
-                    // ポケモン図鑑No.の昇順になるよう並び替え
-                    // TODO: 要素がenumのケースだった場合の実装方法が分からない
-                    self?.pokemons.sort {
-                        guard let pokedexNumber = $0.pokemon else { fatalError("unexpectedError") }
-                        guard let anotherPokedexNumber = $1.pokemon else { fatalError("unexpectedError") }
-                        return pokedexNumber.id < anotherPokedexNumber.id
-                    }
-                    // Setは要素を一意にする為、一度追加されたタイプを自動で省いてくれる。(例: フシギダネが呼ばれると草タイプと毒タイプを取得するので次のフシギソウのタイプは追加されない。
-                    // 結果としてタイプリストの重複を避けることができる
-                    self?.pokemons.forEach {
-                        $0.pokemon?.types.forEach { self?.pokemonTypes.insert($0.type.name) }
-                    }
-
-
-                    // pokemonTypeItemsはlazyプロパティなので初期値が決まる
-                    // 全タイプ対象のItemを追加
-                    self?.pokemonTypeItems.insert(ListItem(pokemonType: "all"), at: 0)
-                    guard let pokemonTypeItems = self?.pokemonTypeItems else { fatalError("unexpectedError") }
-                    guard let pokemons = self?.pokemons else { fatalError("unexpectedError") }
-
-                    self?.view.updateView(pokemonTypeItems: pokemonTypeItems, pokemons: pokemons)
-                }
-            case .failure(let error as URLError):
-                DispatchQueue.main.async {
-                    self?.view.showAlertMessage(errorMessage: error.message)
-                }
-            case .failure:
-                fatalError("unexpectedError")
-            }
-        })
+        fetchPokemons()
     }
 
     // PokemonTypeCellタップ時にViewから呼び出される処理
