d9e4d336a85958f3f5af26b984a9794fc9457808
0102johnny <rt2824.recruit.1022@gmail.com>
2023-07-19 23:52:08 +0900

    [refactor] Index指定で要素にアクセスするとクラッシュのリスクがあり、それ以降のテストもストップしてしまう為、仕様変更。また、空のデータを取得できているかをテストする処理を可読性を改善したものに変更

--- SampleUICollectionViewDiffableDataSourceTests/PokemonListPresenterTests.swift
+++ SampleUICollectionViewDiffableDataSourceTests/PokemonListPresenterTests.swift
@@ -22,15 +22,15 @@ final class PokemonListPresenterTests: XCTestCase {
         wait(for: [expectaion], timeout: 5)
         
         // mockPokemonListViewで定義されているupdateViewHistoryにデータが正しく渡されているかをテスト
-        XCTAssertEqual(mockPokemonListView.updateViewHistory[0][0].name, "pikachu")
+        XCTAssertEqual(mockPokemonListView.updateViewHistory, [PokemonListSampleData().pikachu])
     }
 
-    
+    // 通信処理後、View側で空データになっているかをテスト
     func testEmptyPokemonsList() {
         let expectaion = self.expectation(description: "Viewが更新された")
         // デフォルト引数でnilが入り、二項演算子でPokemonデータがnilである場合は空の配列を返す
         // 1体のポケモンデータが含まれたデータ配列を代入
-        let mockAPI = MockAPI()
+        let mockAPI = MockAPI(mockPokemonData: [])
         let mockPokemonListView = MockPokemonListView(expectationForUpdateView: expectaion)
         let presenter = PokemonListPresenter(view: mockPokemonListView, model: mockAPI)
 
@@ -39,10 +39,9 @@ final class PokemonListPresenterTests: XCTestCase {
         wait(for: [expectaion], timeout: 5)
 
         // mockPokemonListViewで定義されているupdateViewHistoryが空であることをテスト
-        XCTAssertEqual(mockPokemonListView.updateViewHistory[0], [])
+        XCTAssertEqual(mockPokemonListView.updateViewHistory, [[]])
         // こっちの書き方がスマートな気がするがこっちは通らない
 //        XCTAssertTrue(mockPokemonListView.updateViewHistory[0].isEmpty)
-
     }
 
     // 5体のポケモンデータをViewに正しく渡せているかのテスト
@@ -58,12 +57,20 @@ final class PokemonListPresenterTests: XCTestCase {
         wait(for: [expectaion], timeout: 5)
 
         // mockPokemonListViewで定義されているupdateViewHistoryに5つのデータが正しく渡されているかをテスト
-        XCTAssertEqual(mockPokemonListView.updateViewHistory[0].count, 5)
+        XCTAssertEqual(mockPokemonListView.updateViewHistory, [PokemonListSampleData().favoriteFivePokemons])
     }
 
     // エラー発生時のテスト
-    func testCheckError() {
-        // パース処理失敗時のエラーを代入
-        let mockAPI = MockAPI(apiError: .decodingFailed)
-    }
+//    func testCheckError() {
+//        let expectaion = self.expectation(description: "Viewが更新された")
+//
+//        // パース処理失敗時のエラーを代入
+//        let mockAPI = MockAPI(apiError: .decodingFailed)
+//        let mockPokemonListView = MockPokemonListView(expectationForUpdateView: expectaion)
+//        let presenter = PokemonListPresenter(view: mockPokemonListView, model: mockAPI)
+//
+//        // 仮想通信を実行
+//        presenter.viewDidLoad()
+//        wait(for: [expectaion], timeout: 5)
+//    }
 }
