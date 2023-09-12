54fe2851e7af48ffa3d6b839e76e2ceded518925
0102johnny <rt2824.recruit.1022@gmail.com>
2023-09-12 19:33:00 +0900

    [add] DTOにプロジェクトで扱うEntityを生成し、自身の値を渡すメソッドを定義

--- SampleUICollectionViewDiffableDataSource/Models/PokemonDTO.swift
+++ SampleUICollectionViewDiffableDataSource/Models/PokemonDTO.swift
@@ -16,4 +16,18 @@ struct PokemonDTO: Decodable {
      let sprites: Image
      // ポケモンのタイプ
      let types: [TypeEntry]
+
+    ///  プロジェクトで扱うEntityを生成し、自身の値を渡す
+    func convertToPokemon() -> PokemonEntity {
+        // TypeEntryの要素にアクセスし、シーケンスを生成
+        let types = self.types.map { $0.type.name }
+
+        // Pokemon(Entity)インスタンスを生成
+        return PokemonEntity(
+            name: self.name,
+            id: self.id,
+            image: self.sprites.frontImage,
+            types: types
+        )
+    }
  }
